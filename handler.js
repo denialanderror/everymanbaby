4be19e532f1015582097b642ae394cb2270a43b5
Sam Joseph <sam.joseph@blackcatsolutions.co.uk>
2019-09-01 16:39:02 +0100

    Lambda invokes correctly

--- handler.js
+++ handler.js
@@ -5,29 +5,29 @@ const sns = new aws.SNS();
 const url = "https://movieeverymanapi.peachdigital.com/movies/13/195";
 
 module.exports.findAndNotify = async () => {
-  return axios
-    .get("https://movieeverymanapi.peachdigital.com/movies/13/195")
-    .then(({ data }) => {
-      const listings = data.filter(byBabyCinema).map(friendlyFormat);
-      console.log("LISTINGS:" + JSON.stringify(listings));
-      sns
-        .publish({ TopicArn: process.env.topicArn, message: listings })
-        .promise()
-        .then(() => console.log("message published"))
-        .catch(error => console.log("SNS ERROR:" + error));
-      return listings;
-    })
-    .catch(error => console.log("API ERROR:" + error));
+  let response = await axios.get(url);
+  const rawListings = response.data.filter(byBabyCinema);
+  const listings = JSON.stringify(rawListings.map(friendlyFormat));
+  console.log("LISTINGS:" + listings);
+
+  let notification = await sns
+    .publish({ TopicArn: process.env.topicArn, Message: listings })
+    .promise();
+
+  return listings;
 };
 
 const byBabyCinema = film =>
   film.Experiences.map(exp => exp.ExternalId).includes("Baby Club");
 
-const friendlyFormat = ({ Title, Teaser, Sessions }) => ({
-  title: Title,
-  teaser: Teaser,
-  times: babySessionsOnly(Sessions)
-});
+const friendlyFormat = ({ Title, Teaser, Sessions }) => {
+  console.log(Sessions);
+  return {
+    title: Title,
+    teaser: Teaser,
+    times: babySessionsOnly(Sessions)
+  };
+};
 
 const babySessionsOnly = sessions =>
   sessions
@@ -40,3 +40,11 @@ const babySessionsOnly = sessions =>
       )
     )
     .map(event => ({ date: event.date, time: event.time.StartTime }));
+
+const concat = (x, y) => x.concat(y);
+
+const flatMap = (f, xs) => xs.map(f).reduce(concat, []);
+
+Array.prototype.flatMap = function(f) {
+  return flatMap(f, this);
+};
